9612a00108e56b049dbb822f0ec6085c694b2a37
solo206 <jsolk206@gmail.com>
2014-08-21 21:40:17 -0700

    non-interactive proto

---
+++ app.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    function Player(score,time){
+    	this.score=score;
+    	this.times=time;
+    }
+    var player=new Player(0,0:00);
+    $(".intro").click(function(){
+        $(".circleHolder").addClass("outer1");
+        $(".circle").addClass("inner1");
+        $(".logo,.mousetext,.pretitle,.title,.top").hide();
+        $(".rectLeft").css({"display":"block"});
+        $(".main").removeClass("intro");
+    }); 
+    $(".instructStart").click(function(){
+    	$(".rectLeft").css({"width":"45%","float":"left","margin-left":"3%"});
+    	$(".instructions").hide();
+    	$(".questionholder").css({"display":"block"});
+    	$(".rectRight").css({"display":"block"});
+    });
+    $(".end").click(function(){
+    	$(".rectLeft").css({"width":"35%"});
+    	$(".rectRight").css({"width":"55%"})
+    	$(".questionholder").hide();
+    	$(".imageHolder").hide();
+    	$(".gameover").css({"display":"block"});
+    	$(".social").css({"display":"block"});
+    });
+    // $(".restart").click(function(){
+    // 	$(".rectLeft").css({"display":"none"});
+    // 	$(".rectRight").css({"display":"none"});
+    // 	$(".circleHolder").removeClass("outer1");
+    // 	$(".logo,.mousetext,.pretitle,.title,.top").show();
+    // 	$(".circle").removeClass("inner1");
+    // 	$(".main").addClass("intro");
+    // 	$(".gameover").css({"display":"none"});
+    // 	$(".social").css({"display":"none"});
+    // 	$(".rectLeft").hide();
+
+    // });
+});
